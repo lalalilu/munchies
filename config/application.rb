74c2b108de0d5a1687d8830a962d87caa34cd8e7
Lulu Li <lilulu004@gmail.com>
2016-08-15 16:03:34 +0000

    Added custom fonts to brand

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Munchies
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add the font path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
