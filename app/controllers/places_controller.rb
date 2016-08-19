5d8f0c1907d1f8c48a274c47dd8dd36447df3ecd
Lulu Li <lilulu004@gmail.com>
2016-08-19 20:36:02 +0000

    Destroy permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.order(:name).page(params[:page]).per(5)
@@ -31,13 +31,17 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
