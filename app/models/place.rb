96834a1a4339c2e1445c0b515583aeb8ca45a198
Lulu Li <lilulu004@gmail.com>
2016-08-20 12:41:40 +0000

    Added mroe validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :address, :description, presence: true
+  validates :name, length: { minimum: 3 }
 end
