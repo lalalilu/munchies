d73960560fc79e839d5dd6b6357587a07d0361e8
Lulu Li <lilulu004@gmail.com>
2016-09-01 18:18:04 +0000

    Adjusted the mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@munchiesapp.com"
 
-  def comment_added
-    mail(to: "lilulu004@gmail.com", subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
   end
 end
