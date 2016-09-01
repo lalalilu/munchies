4b8a8ee56675efed57b2971138f727e24f2cf437
Lulu Li <lilulu004@gmail.com>
2016-09-01 19:26:09 +0000

    Send email on comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  #config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "example.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_ADDRESS'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
