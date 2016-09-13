393fb4faef2a15af5886483ea94d6045e1a37c47
Lulu Li <lilulu004@gmail.com>
2016-09-13 19:55:57 +0000

    Added dependent destroy to photos

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode
